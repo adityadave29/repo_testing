fc076ddab5a2f4d6c8dcc48d51667577eabbe69c
Aditya Dave <107619782+adityadave29@users.noreply.github.com>
2026-01-27 11:03:14 +0530

    Modify output in testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 int main(){
     int n = 4;
-    cout << n << endl;
-    cout << 2*n << endl;
-    cout << 3*n << endl;
-    cout << 4*n << endl;
+    cout << 4 << endl;
     return 0;
 }
